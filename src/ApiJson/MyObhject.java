72336372005c9ee5c018c5013cc88c46585eb625
IbrahimSaid1 <squ118977@gmail.com>
2023-03-19 16:38:27 +0400

    Api

---
+++ src/ApiJson/MyObhject.java
@@ -0,0 +1,12 @@
+package ApiJson;
+
+import java.util.List;
+
+public class MyObhject {
+
+	List<String> domains [];
+	String country;
+	List<String> web_pages [];
+	String name;
+	String alpha_two_code;
+}
